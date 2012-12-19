eb08438e69bdc767d923a92911b4d425c53e540e
Chris Roberts <chris.roberts@wotifgroup.com>
2012-12-19 14:04:40 +1000

    Wot the bootstrap?

--- src/com/wotriders/GenerateHTML.groovy
+++ src/com/wotriders/GenerateHTML.groovy
@@ -8,41 +8,54 @@ class GenerateHTML {
         new File("index.html").withWriter { writer ->
         writer.write '''
 <!DOCTYPE html>
-<html>
-<head>
-<link href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css" rel="stylesheet" media="screen">
-<style>
-        .alt { background-color: #eee }
-</style>
-</head>
+<!--[if lt IE 7]>      <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Wot Riders</title>
+        <meta name="viewport" content="width=device-width">
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:800italic" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
 <body>
 
-<div style="float:left; width:420px; height:405px; overflow:auto">
-<table>
-<tr>
-        <td>Segment</td>
-        <td>Leader</td>
-        <td>Time</td>
-</tr>
-'''
-        int zebra = 0
-        new File("segments").eachFile {
-            
-            def segment = new JsonSlurper().parseText(it.text)
-            def best = segment.efforts.min { it.elapsedTime }
-            writer.write """
-<tr ${zebra % 2 == 0 ? '' :'class="alt"'}>
-    <td><a href="http://app.strava.com/segments/${segment.segment.id}/embed" target="embedme">${segment.segment.name}</a></td>
-    <td>${best.athlete.name}</td>
-    <td>${best.elapsedTime} secs</td>
-</tr>
-"""
-            zebra++
-        }
-        writer.write '''
-</table>
-</div>
-<iframe name="embedme" height='405' width='590' frameborder='0' allowtransparency='true' scrolling='no' src=''></iframe>
+    <div class="container">
+
+        <header>
+            <h1>Wot Riders</h1>
+        </header>
+
+        <iframe name="embedme" height='405' width='100%' frameborder='0' allowtransparency='true' scrolling='no' src=''></iframe>
+
+        <table class="table table-striped">
+        <tr>
+                <th>Segment</th>
+                <th>Leader</th>
+                <th>Time</th>
+        </tr>
+        '''
+                new File("segments").eachFile {
+                    
+                    def segment = new JsonSlurper().parseText(it.text)
+                    def best = segment.efforts.min { it.elapsedTime }
+                    writer.write """
+        <tr>
+            <td><a href="http://app.strava.com/segments/${segment.segment.id}/embed" target="embedme">${segment.segment.name}</a></td>
+            <td>${best.athlete.name}</td>
+            <td>${best.elapsedTime} secs</td>
+        </tr>
+        """
+                }
+                writer.write '''
+        </table>
+
+    </div>
+
 </body>
 </html>
 '''
